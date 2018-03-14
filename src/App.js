e0a7ec0df4462d09c788912ef0a9229b3f3214df
Christopher Bauer <chris@DEVIMAGE-LAPTOP.enterprisecb.com>
2018-03-13 23:25:25 -0400

    Add bootstrap cdns/move image to assets folder

--- src/App.js
+++ src/App.js
@@ -11,24 +11,31 @@ class App extends Component {
     return squares.map((square, index) => <p data-value={square.value} className="box"></p>);
   }
   renderSkills(skills) {
-      const skillInfo = [{ "name": "Speed Sharpening", "max": 3},{ "name": "Weakness Exploit", "max": 3}];
       return (
         skills.map(function(item, index) {
         return (
-          <div className="skills">
-            <h3>{item.name}</h3>
-            {this.renderBox(item.value, 5)}
+          <div className="skills row">
+            <div className="col-sm-2">
+              <h3>{item.name}</h3>
+            </div>
+            <div className="col-sm-10 skill-display">
+              {this.renderBox(item.value, 5)}
+            </div>
           </div>
         );
       }, this)
-    );
+  );
   }
   renderArmor(type, rarity, name, skills) {
     return (
     <div class="armor">
-      <div class="icon">
-        <i className={type + " rarity-" + rarity}></i>
-        <p>{name}</p>
+      <div class="icon row">
+        <div className="col-sm-2">
+          <i className={type + " rarity-" + rarity}></i>
+        </div>
+        <div className="col-sm-10">
+          <p>{name}</p>
+        </div>
       </div>
       {this.renderSkills(skills)}
     </div>
@@ -41,14 +48,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          <h2>Popular Armor Pieces</h2>
-          {this.renderArmor("helmet",8,"Dragon King Eyepatch", [{ "name":"Weakness Exploit", "value": 2 }])}
-          {this.renderArmor("chest",7,"Odogaron Mail A", [ { "name": "Speed Sharpening", "value": 2 }, { "name": "Bleeding Resistance", "value": 1 } ])}
-          {this.renderArmor("arms",7,"Uragaan Vambraces B", [ { "name": "Guard", "value": 1 } ])}
-          {this.renderArmor("waist",8,"Xeno'jiiva Spine A", [ { "name": "Blight Resistance", "value": 2 }, { "name": "Special Ammo Boost", "value": 1 } ])}
-          {this.renderArmor("legs",5,"Gastodon Alpha Armor A", [ { "name": "Flinch Free", "value": 1 }, { "name": "Blast Resistance", "value": 1 } ])}
-        </p>
+        <div className="App-main container">
+          <div className="row">
+            <h2>Popular Armor Pieces</h2>
+          </div>
+          <div className="row">
+            {this.renderArmor("helmet",8,"Dragon King Eyepatch", [{ "name":"Weakness Exploit", "value": 2 }])}
+            {this.renderArmor("chest",7,"Odogaron Mail A", [ { "name": "Speed Sharpening", "value": 2 }, { "name": "Bleeding Resistance", "value": 1 } ])}
+            {this.renderArmor("arms",7,"Uragaan Vambraces B", [ { "name": "Guard", "value": 1 } ])}
+            {this.renderArmor("waist",8,"Xeno'jiiva Spine A", [ { "name": "Blight Resistance", "value": 2 }, { "name": "Special Ammo Boost", "value": 1 } ])}
+            {this.renderArmor("legs",5,"Gastodon Alpha Armor A", [ { "name": "Flinch Free", "value": 1 }, { "name": "Blast Resistance", "value": 1 } ])}
+          </div>
+        </div>
       </div>
     );
   }
