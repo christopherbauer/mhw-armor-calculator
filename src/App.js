db46b229a68e30af3678de6a8de5be96f955217b
Christopher Bauer <chris@DEVIMAGE-LAPTOP.enterprisecb.com>
2018-03-14 21:17:42 -0400

    Setup the concept of drawing in skills by name in order to display context information

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { isUndefined } from 'util';
 
 class App extends Component {
   renderBox(value, max) {
@@ -11,15 +12,23 @@ class App extends Component {
     return squares.map((square, index) => <p data-value={square.value} className="box"></p>);
   }
   renderSkills(skills) {
+    var skillInfo = [];
+    skillInfo["Blast Resistance"] = { "max": 3 };
+    skillInfo["Blight Resistance"] = { "max": 3 };
+    skillInfo["Flinch Free"] = { "max": 3 };
+    skillInfo["Guard"] = { "max": 5 };
+    skillInfo["Speed Sharpening"] = { "max": 3 };
+    skillInfo["Special Ammo Boost"] = { "max": 1 };
+    skillInfo["Weakness Exploit"] = { "max": 3 };
       return (
         skills.map(function(item, index) {
         return (
           <div className="skills row">
             <div className="col-sm-2">
-              <h3>{item.name}</h3>
+              <p>{item.name}</p>
             </div>
             <div className="col-sm-10 skill-display">
-              {this.renderBox(item.value, 5)}
+              {this.renderBox(item.value, skillInfo[item.name] !== undefined ? skillInfo[item.name].max : 0)}
             </div>
           </div>
         );
@@ -28,13 +37,16 @@ class App extends Component {
   }
   renderArmor(type, rarity, name, skills) {
     return (
-    <div class="armor">
+    <div class="armor container">
       <div class="icon row">
-        <div className="col-sm-2">
+        <div className="col-sm-1">
+          <input type="checkbox" />
+        </div>
+        <div className="col-sm-1">
           <i className={type + " rarity-" + rarity}></i>
         </div>
         <div className="col-sm-10">
-          <p>{name}</p>
+          <h3>{name}</h3>
         </div>
       </div>
       {this.renderSkills(skills)}
@@ -52,7 +64,7 @@ class App extends Component {
           <div className="row">
             <h2>Popular Armor Pieces</h2>
           </div>
-          <div className="row">
+          <div className="armor-list">
             {this.renderArmor("helmet",8,"Dragon King Eyepatch", [{ "name":"Weakness Exploit", "value": 2 }])}
             {this.renderArmor("chest",7,"Odogaron Mail A", [ { "name": "Speed Sharpening", "value": 2 }, { "name": "Bleeding Resistance", "value": 1 } ])}
             {this.renderArmor("arms",7,"Uragaan Vambraces B", [ { "name": "Guard", "value": 1 } ])}
